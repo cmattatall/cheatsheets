aa600312a4ab5f3e8186a5093f65a98bc6f55701
Carl Mattatall <cmattatall2@gmail.com>
2021-11-26 11:22:18 -0400

    [Fix] formatting in cdk init function alias

--- python/venv-setup.sh
+++ python/venv-setup.sh
@@ -20,20 +20,16 @@ function python_env_init() {
 # Initializes an AWS cdk stack project (python)
 # and sources a python virtual environment
 function cdk_python_env_init() {
-  if [ -f requirements.txt ]; then
-    if [ $(dpkg --list | grep env | grep virtual | grep python | awk '{print $2}')  -eq "python3-virtualenv" ]; then
-      if [ $(which cdk) -eq 0 ]; then
-        cdk init app --language python
-        source venv/bin/activate
-        python -m pip install -r requirements.txt
-      else
-        echo "program \"cdk\" not found on disk, please install it first"
-      fi
-         else
-      echo "python3-virtualenv does not seem to be installed. Please install it first"
+  if [ $(dpkg --list | grep env | grep virtual | grep python | awk '{print $2}')  -eq "python3-virtualenv" ]; then
+    if [ $(which cdk) -eq 0 ]; then
+      cdk init app --language python
+      source venv/bin/activate
+      python -m pip install -r requirements.txt
+    else
+      echo "program \"cdk\" not found on disk, please install it first"
     fi
-  else
-    echo "No requirements.txt file present in the current directory"
+       else
+    echo "python3-virtualenv does not seem to be installed. Please install it first"
   fi
 }
 
