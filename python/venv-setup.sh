7e370bc0c4fdd32f4a24a83c2c229782239d9ca5
Carl Mattatall <cmattatall2@gmail.com>
2021-11-26 11:34:39 -0400

    [Refactor] pyenv and cdk env init functions

--- python/venv-setup.sh
+++ python/venv-setup.sh
@@ -1,35 +1,59 @@
 
 # Add this to your bashrc
 
-
 function python_env_init() {
-  if [ -f requirements.txt ]; then
-    if [ $(dpkg --list | grep env | grep virtual | grep python | awk '{print $2}')  -eq "python3-virtualenv" ]; then
-      virtualenv -p /usr/bin/python3 venv
-      source venv/bin/activate
-      python -m pip install -r requirements.txt
+    set +e
+    dpkg --list | grep env | grep virtual | grep python | awk '{print $2}' > /dev/null
+    if [ "$?" -eq "0" ]; then
+        which virtualenv
+        if [ "$?" -eq "0" ]; then
+            $(which virtualenv) -p /usr/bin/python3 venv
+            source venv/bin/activate
+            if [ -f requirements.txt ]; then
+                echo "Found requirements.txt file. Installing python packages now..."
+                python -m pip install -r requirements.txt
+                if [ "$?" -eq "0" ]; then
+                    echo "Ok."
+                else
+                    echo "Error installing python packages from requirements.txt"
+                fi
+            else
+                echo "No requirements.txt file present in the current directory"
+            fi
+        else
+            echo "Could not find virtualenv executable in PATH"
+        fi
     else
-      echo "python3-virtualenv does not seem to be installed. Please install it first"
+        echo "python3-virtualenv does not seem to be installed. Please install it first"
     fi
-  else
-    echo "No requirements.txt file present in the current directory"
-  fi
+    set -e
 }
 
 
 # Initializes an AWS cdk stack project (python)
 # and sources a python virtual environment
 function cdk_python_env_init() {
-  if [ $(dpkg --list | grep env | grep virtual | grep python | awk '{print $2}')  -eq "python3-virtualenv" ]; then
-    if [ $(which cdk) -eq 0 ]; then
-      cdk init app --language python
-      source venv/bin/activate
-      python -m pip install -r requirements.txt
+    dpkg --list | grep env | grep virtual | grep python | awk '{print $2}' > /dev/null
+    if [ "$?" -eq "0" ]; then
+        echo "Looking for cdk ... "
+        which cdk > /dev/null
+        if [ "$?" -eq "0" ]; then
+            $(which cdk) init app --language python
+            source venv/bin/activate
+            if [ -f requirements.txt ]; then
+                python -m pip install -r requirements.txt
+                if [ "$?" -eq "0" ]; then
+                    echo "Ok."
+                else
+                    echo "Error installing python packages from requirements.txt"
+                fi
+            else
+                echo "No requirements.txt file found! Could not install python packages"
+            fi
+        else
+            echo "program \"cdk\" not found on disk, please install it first"
+        fi
     else
-      echo "program \"cdk\" not found on disk, please install it first"
+        echo "python3-virtualenv does not seem to be installed. Please install it first"
     fi
-       else
-    echo "python3-virtualenv does not seem to be installed. Please install it first"
-  fi
 }
-
