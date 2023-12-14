106d7d7861f99f8d6055b98a68d4c4d93034c3ac
Carl Mattatall <carl.mattatall@rimot.io>
2023-12-14 10:26:37 -0400

    Update git-pull-all.sh

--- scripts/git-pull-all.sh
+++ scripts/git-pull-all.sh
@@ -7,6 +7,11 @@ for dir in ${dirs[@]}; do
 	REPO_DIR=$(dirname $dir)
 	echo "Pulling latest changes to $REPO_DIR ..."
 	git -C "$REPO_DIR" pull
+
+	# TODO: fix me. This is the proper way of doing it but the implementation is currently broken. We want to track an upstream branch if we are not tracking, then merge from origin/$BRANCH -> $BRANCH for each out of date branch
+	#git -C "$REPO_DIR" branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git -C "$REPO_DIR" branch --track "${remote#origin/}" "$remote" || true; done
+	#git -C "$REPO_DIR" fetch --all
+	#git -C "$REPO_DIR" pull --all
 done
 
 
